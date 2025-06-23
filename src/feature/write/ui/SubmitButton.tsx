577e038621640e9664c286386255f874a90d0629
lkj1313 <smm45108@gmail.com>
2025-06-23 21:58:18 +0900

    refactor : write-page ui 쪼개기

---
+++ src/feature/write/ui/SubmitButton.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/common';
+
+const SubmitButton = () => {
+  return (
+    <section className="flex justify-end">
+      <Button variant="primary" type="submit">
+        작성완료
+      </Button>
+    </section>
+  );
+};
+
+export default SubmitButton;
